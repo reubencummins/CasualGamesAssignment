3791fe28cefaaf782f3b4c143db88a66acd3129e
reubencummins <reubencummins@gmail.com>
2016-01-24 21:30:24 +0000

    Player Authentication

---
+++ CasualGamesAssignment/GameObjectModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebAPIAuthenticationClient
+{
+    public class PlayerProfile
+        {
+        public string ID;
+        public string Email;
+        public string UserName;
+        }
+
+
+    public class GameScoreObject
+    {
+        public string PlayerID { get; set; }
+
+        public int Score { get; set; }
+
+    }
+
+
+}
